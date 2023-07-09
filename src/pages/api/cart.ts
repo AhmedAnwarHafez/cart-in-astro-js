e2420562454a21661c498acef497afe20dc78613
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-09 12:37:24 +1200

    store cart item

--- src/pages/api/cart.ts
+++ src/pages/api/cart.ts
@@ -1,7 +1,20 @@
 import type { APIRoute } from 'astro'
+import { PrismaClient } from '@prisma/client'
 
-export const post: APIRoute = ({ request, redirect }) => {
-  console.log('post route hit')
+export const post: APIRoute = async ({ request, redirect }) => {
+  const prisma = new PrismaClient()
+
+  const form = await request.formData()
+  const productId = form.get('productId')?.valueOf()
+
+  if (productId) {
+    await prisma.cart.create({
+      data: {
+        productId: Number(productId),
+        quantity: 1,
+      },
+    })
+  }
 
   return redirect('/cart', 301)
 }
